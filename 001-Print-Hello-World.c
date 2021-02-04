e77ef945f89bf3a8f12c5e669bc3a77c2352e986
Surya Karmakar <74616729+SuryaKarmakar@users.noreply.github.com>
2021-02-04 14:16:15 +0530

    Add files via upload

---
+++ 001-Print-Hello-World.c
@@ -0,0 +1,12 @@
+//#Pre-Processor directives <Header-file>
+#include<stdio.h>
+
+//Main() function with integer return type 
+int main(){
+	//printf() is built-in functions,that show the output
+	printf("hello world!"); 
+	return 0;
+}
+
+
+		
