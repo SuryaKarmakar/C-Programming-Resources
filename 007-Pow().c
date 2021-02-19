167f253fa72103dabda1775c8deeec22b6df70da
Surya Karmakar <74616729+SuryaKarmakar@users.noreply.github.com>
2021-02-19 22:03:13 +0530

    Add files via upload

---
+++ 007-Pow().c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>    //using math header file
+int main(){
+	int a;
+	// pow(x,y) => x to the power y
+	a=pow(3,5);
+	printf("3 to the power 5 is : %d",a);
+	return 0;
+}
