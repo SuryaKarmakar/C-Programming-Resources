13157ec51814c644e3831dac9513b4ed30068c24
Surya Karmakar <74616729+SuryaKarmakar@users.noreply.github.com>
2021-02-19 21:27:14 +0530

    Add files via upload

---
+++ 005-Receiving-Input-Scanf().c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("enter a number : ");
+	// using scanf() function we can receive user input
+	// & => Address of operator  
+	scanf("%d",&a);
+	printf("You enter %d this number",a);
+	return 0;
+}
