2fc1e1ea7760ef3f5c3fbf602823e69a5a55790d
Surya Karmakar <74616729+SuryaKarmakar@users.noreply.github.com>
2021-02-20 22:26:54 +0530

    Add files via upload

---
+++ 016-AND-Operator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	char adharCard = 'y';
+	char panCard = 'n';
+
+	//if both of the conditions are ture then it will execute
+	if ((adharCard == 'y') && (panCard == 'y'))
+		printf("You are eligible to join\n");
+	else
+		printf("you are not eligible to join\n");
+return 0;
+}
